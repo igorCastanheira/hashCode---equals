f917ff96179fb117a8c88da4b063232ae2a194c6
Igor Castanheira <igor.castanheira.dev@gmail.com>
2021-05-03 12:17:44 -0300

    Example of HashSet

---
+++ src/application/Program.java
@@ -0,0 +1,25 @@
+package application;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class Program {
+
+	public static void main(String[] args) {
+		//Set<String> set = new HashSet<>(); //HashSet a ordem não importa;
+		Set<String> set = new TreeSet<>(); //TreeSet é ordenado um pouco mais lento que Hash
+		set.add("TV");
+		set.add("Notebook");
+		set.add("Table");
+
+		//set.remove("Tablet");
+		//set.removeIf(x -> x.length() >= 6); // Remova item x tal que -> x.length >= 3
+		set.removeIf(x -> x.charAt(0)== 'T');
+		for (String p : set) {
+
+			System.out.println(p);
+		}
+	}
+
+}
